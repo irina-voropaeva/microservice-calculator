8b01f546145d8d2a5a0e7508911eb0e408ef8eaf
Irina Voropayeva <irina.voropayeva@dataart.com>
2020-12-20 10:18:32 +0200

    Calculator microservice added (Draft)

---
+++ CalculatorMicroservice/CalculatorMicroservice/CalculateController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorMicroservice
+{
+    public class CalculateController : Controller
+    {
+        private string authUrl = "http://localhost:5000/TokenValidation/GetIsTokenValid";
+        
+        public IActionResult Calculate(string type, string operation, string number1, string number2, string token)
+        {
+            if (IsAuthenticated(token))
+            {
+                return Ok("Hello))");
+            }
+            else
+            {
+                return Unauthorized("Please authorize!");
+            }
+        }
+
+        private bool IsAuthenticated(string token)
+        {
+            return true;
+        }
+    }
+}
